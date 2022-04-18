b2105b0fcb075a66a974900b34e1ab4741831701
TreefysGit <morarusergiu2010@gmail.com>
2022-04-18 21:53:31 +0300

    Upgrades + Different folder locations
    Added upgrades, only 3, will add more when game is gonna be done.
    Moved images in proper directories.
    Published on 18 April 2022, 21:53 EEST.

--- game/main.js
+++ game/main.js
@@ -54,6 +54,71 @@ var building = {
             this.cost[index] = Math.ceil(this.cost[index] * 1.10);
             display.updateScore();
             display.updateShop();
+            display.updateUpgrades();
+        }
+    }
+};
+
+var upgrade = {
+    name: [
+        "Stone Fingers",
+        "Iron Fingers",
+        "Stone Clicker"
+    ],
+    description: [
+        "Cursors are twice as efficient.",
+        "Cursors are twice as efficient.",
+        "Your clicks are twice as efficient."
+    ],
+    image: [
+        "stonefingers.png",
+        "ironfingers.png",
+        "stoneclicker.png"
+    ],
+    type: [
+        "building",
+        "building",
+        "click"
+    ],
+    cost: [
+        300,
+        500,
+        300
+    ],
+    buildingIndex: [
+       0,
+       0,
+       -1
+    ],
+    requirement: [
+        1,
+        5,
+        1
+    ],
+    bonus: [
+        2,
+        2,
+        2
+    ],
+    purchased: [false, false, false],
+
+    purchase: function(index) {
+        if (!this.purchased[index] && game.score >= this.cost[index]) {
+            if (this.type[index] == "building" && building.count[this.buildingIndex[index]] >= this.requirement[index]) {
+                game.score -= this.cost[index];
+                building.income[this.buildingIndex[index]] *= this.bonus[index];
+                this.purchased[index] = true;
+
+                display.updateUpgrades();
+                display.updateScore();
+            } else if (this.type[index] == "click" && game.totalClicks >= this.requirement[index]) {
+                game.score -= this.cost[index];
+                game.clickValue *= this.bonus[index];
+                this.purchased[index] = true;
+
+                display.updateUpgrades();
+                display.updateScore();
+            }
         }
     }
 };
@@ -62,13 +127,26 @@ var display = {
     updateScore: function() {
         document.getElementById("score").innerHTML = game.score;
         document.getElementById("scorepersecond").innerHTML = game.getScorePerSecond();
-        document.title = score + " muffins - Muffin Clicker: The muffining";
+        document.title = game.score + " muffins - Muffin Clicker: The muffining";
     },
 
     updateShop: function() {
         document.getElementById("shopContainer").innerHTML = "";
         for (i = 0; i < building.name.length; i++) {
-            document.getElementById("shopContainer").innerHTML += '<table class="shopButton unselectable" onclick="building.purchase('+i+')"><tr><td id="image"><img src="game/images/'+building.image[i]+'"></td><td id="nameAndCost"><p>'+building.name[i]+'</p><p><span>'+building.cost[i]+'</span> muffins</p></td><td id="amount"><span>'+building.count[i]+'</span></td></tr></table>';
+            document.getElementById("shopContainer").innerHTML += '<table class="shopButton unselectable" onclick="building.purchase('+i+')"><tr><td id="image"><img src="game/buildings/'+building.image[i]+'"></td><td id="nameAndCost"><p>'+building.name[i]+'</p><p><span>'+building.cost[i]+'</span> muffins</p></td><td id="amount"><span>'+building.count[i]+'</span></td></tr></table>';
+        }
+    },
+
+    updateUpgrades: function() {
+        document.getElementById("upgradeContainer").innerHTML = "";
+        for (i = 0; i < upgrade.name.length; i++) {
+            if (!upgrade.purchased[i]) {
+                if (upgrade.type[i] == "building" && building.count[upgrade.buildingIndex[i]] >= upgrade.requirement[i]) {
+                    document.getElementById("upgradeContainer").innerHTML += '<img src="game/upgrades/building/'+upgrade.image[i]+'" title="'+upgrade.name[i]+' &#10; '+upgrade.description[i]+' &#10; ('+upgrade.cost[i]+' muffins)" onclick="upgrade.purchase('+i+')">';
+                } else if (upgrade.type[i] == "click" && game.totalClicks >= upgrade.requirement[i]) {
+                    document.getElementById("upgradeContainer").innerHTML += '<img src="game/upgrades/click/'+upgrade.image[i]+'" title="'+upgrade.name[i]+' &#10; '+upgrade.description[i]+' &#10; ('+upgrade.cost[i]+' muffins)" onclick="upgrade.purchase('+i+')">';
+                }
+            }
         }
     }
 };
@@ -82,7 +160,8 @@ function saveGame() {
         version: game.version,
         buildingCount: building.count,
         buildingIncome: building.income,
-        buildingCost: building.cost
+        buildingCost: building.cost,
+        upgradePurchased: upgrade.purchased
     };
     localStorage.setItem("gameSave", JSON.stringify(gameSave));
 }
@@ -99,11 +178,6 @@ function loadGame() {
                 building.count[i] = savedGame.buildingCount[i];
             }
         }
-        if (typeof savedGame.buildingCount !== "undefined") {
-            for (i = 0; i < savedGame.buildingCount.length; i++) {
-                building.count[i] = savedGame.buildingCount[i];
-            }
-        }
         if (typeof savedGame.buildingIncome !== "undefined") {
             for (i = 0; i < savedGame.buildingIncome.length; i++) {
                 building.income[i] = savedGame.buildingIncome[i];
@@ -112,6 +186,11 @@ function loadGame() {
         if (typeof savedGame.buildingCost !== "undefined") {
             for (i = 0; i < savedGame.buildingCost.length; i++) {
                 building.cost[i] = savedGame.buildingCost[i];
+            }
+        }
+        if (typeof savedGame.upgradePurchased !== "undefined") {
+            for (i = 0; i < savedGame.upgradePurchased.length; i++) {
+                upgrade.purchased[i] = savedGame.upgradePurchased[i];
             }
         }
     }
@@ -125,9 +204,15 @@ function resetGame() {
     }
 }
 
+document.getElementById("clicker").addEventListener("click", function() {
+    game.totalClicks++;
+    game.addToScore(game.clickValue);
+}, false);
+
 window.onload = function() {
     loadGame();
     display.updateScore();
+    display.updateUpgrades();
     display.updateShop();
 };
 
@@ -136,6 +221,11 @@ setInterval(function() {
     game.totalScore += game.getScorePerSecond();
     display.updateScore();
 }, 1000); // 1000ms = 1 second
+
+setInterval(function() {
+    display.updateScore();
+    display.updateUpgrades();
+}, 10000); // 10000ms = 10 seconds
 
 setInterval(function () {
     saveGame();
