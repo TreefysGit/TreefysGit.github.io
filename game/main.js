58b32348e8a6568f707dff5af65f3877982a1872
TreefysGit <morarusergiu2010@gmail.com>
2022-04-19 13:11:34 +0300

    Achievements!
    Added achievement system.
    Published on 19 April 2022, 13:11 EEST.

--- game/main.js
+++ game/main.js
@@ -123,6 +123,44 @@ var upgrade = {
     }
 };
 
+var achievement = {
+    name: [
+        "Stone Fingers",
+        "A Humble Start",
+        "Fingertastic"
+    ],
+    description: [
+        "Buy 1 cursor.",
+        "Gather 1 muffin.",
+        "Click the big muffin 1 time."
+    ],
+    image: [
+        "stonefingers.png",
+        "ahumblestart.png",
+        "fingertastic.png"
+    ],
+    type: [
+        "building",
+        "score",
+        "click"
+    ],
+    requirement: [
+        1,
+        1,
+        1
+    ],
+    objectIndex: [
+        0,
+        -1,
+        -1
+    ],
+    awarded: [false, false, false],
+
+    earn: function(index) {
+        this.awarded[index] = true;
+    }
+};
+
 var display = {
     updateScore: function() {
         document.getElementById("score").innerHTML = game.score;
@@ -148,6 +186,15 @@ var display = {
                 }
             }
         }
+    },
+
+    updateAchievements: function() {
+        document,getElementById("achievementContainer").innerHTML = "";
+        for (i = 0; i < achievement.name.length; i++) {
+            if (achievement.awarded[i]) {
+                document,getElementById("achievementContainer").innerHTML += '<img src="game/achievements/'+achievement.image[i]+' title="'+achievement.name[i]+' &#10; '+achievement.description[i]+'">';
+            }
+        }
     }
 };
 
@@ -161,7 +208,8 @@ function saveGame() {
         buildingCount: building.count,
         buildingIncome: building.income,
         buildingCost: building.cost,
-        upgradePurchased: upgrade.purchased
+        upgradePurchased: upgrade.purchased,
+        achievementAwarded: achievement.awarded
     };
     localStorage.setItem("gameSave", JSON.stringify(gameSave));
 }
@@ -193,6 +241,11 @@ function loadGame() {
                 upgrade.purchased[i] = savedGame.upgradePurchased[i];
             }
         }
+        if (typeof savedGame.achievementAwarded !== "undefined") {
+            for (i = 0; i < savedGame.achievementAwarded.length; i++) {
+                achievement.awarded[i] = savedGame.achievementAwarded[i];
+            }
+        }
     }
 }
 
@@ -213,13 +266,21 @@ window.onload = function() {
     loadGame();
     display.updateScore();
     display.updateUpgrades();
+    display.updateAchievements();
     display.updateShop();
 };
 
 setInterval(function() {
+    for (i = 0; i < achievement.name.length; i++) {
+        if (achievement.type[i] == "score" && game.totalScore >= achievement.requirement[i]) achievement.earn(i);
+        else if (achievement.type[i] == "click" && game.totalClicks >= achievement.requirement[i]) achievement.earn(i);
+        else if (achievement.type[i] == "building" && building.count[achievement.objectIndex[i]] >= achievement.requirement[i]) achievement.earn(i);
+    }
+    
     game.score += game.getScorePerSecond();
     game.totalScore += game.getScorePerSecond();
     display.updateScore();
+    display.updateAchievements();
 }, 1000); // 1000ms = 1 second
 
 setInterval(function() {
